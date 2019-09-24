9775fc1ac9ca84260d8903d0ae3b9ccfda3bfc99
Tyler Porter <hello@cleverladder.com>
2019-09-24 07:41:23 -0700

    Initial commit

---
+++ u-group-code-challenge.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: U Group Code Challenge
+ * Plugin URI: https://u.group
+ * Description: Easily add a project block
+ * Version: 1.0
+ * Author: Tyler Porter
+ * Author URI: https://cleverladder.co
+ *
+ */
